a0f8fbe2f66c41f9879fdbae7e755cd18775aa7f
16Maarten <176maarten@gmail.com>
2023-06-07 16:45:21 +0200

    first version interaction

--- Serious-game/Assets/Scripts/GameInput.cs
+++ Serious-game/Assets/Scripts/GameInput.cs
@@ -2,14 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
     private PlayerInputActions _playerInputActions;
+    public event EventHandler OnInteract;
     private void Awake()
     {
         _playerInputActions = new PlayerInputActions();
         _playerInputActions.Player.Enable();
+        _playerInputActions.Player.Interact.performed += OnInteractPerformed;
+    }
+
+    private void OnInteractPerformed(InputAction.CallbackContext obj)
+    {
+        OnInteract?.Invoke(this, EventArgs.Empty);
     }
 
     public Vector2 GetMovementVectorNormalized()
@@ -17,4 +25,5 @@ public class GameInput : MonoBehaviour
         Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
         return inputVector.normalized;
     }
+    
 }
