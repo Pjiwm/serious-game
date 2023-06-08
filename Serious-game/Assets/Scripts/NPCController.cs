f98def63104bcfc9c91c8b2de001b9a26f0df8d4
Stijn van Houwelingen <stijn@van-houwelingen.net>
2023-06-08 12:12:52 +0200

    refactored interactions to use circleOverlap function

--- Serious-game/Assets/Scripts/NPCController.cs
+++ Serious-game/Assets/Scripts/NPCController.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 
-public class NPCController : MonoBehaviour
+public class NPCController : MonoBehaviour, IInteractable
 {
     private Animator animator;
-    // Start is called before the first frame update
-    void Start()
+
+    private static readonly int IsWalking = Animator.StringToHash("isWalking");
+    
+    private void Start()
     {
         animator = GetComponent<Animator>();
-        animator.SetBool("isWalking", true);
+        animator.SetBool(IsWalking, true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Select()
     {
+        Debug.Log("Could you help me?");
+    }
 
+    public void Interact()
+    {
+        Debug.Log("Hello! I am looking for my dog. Have you seen him?");
     }
 }
