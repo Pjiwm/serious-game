b51221b12afd3b53df189cffe38c053d0b8cf215
Pjiwm <pim.munne@gmail.com>
2023-06-20 11:54:26 +0200

    store playerrefs in emojishooter

--- Serious-game/Assets/Scripts/EmojiShooter/ObstacleSpawner.cs
+++ Serious-game/Assets/Scripts/EmojiShooter/ObstacleSpawner.cs
@@ -8,11 +8,13 @@ public class ObstacleSpawner : MonoBehaviour
     public float spawnTime = 2f;
     private float spawnTimeLeft = 0;
 
+    public int spawnRange = 4;
+
     void Update()
     {
         if (spawnTimeLeft <= 0)
         {
-            int objectCount = Random.Range(1, 4);
+            int objectCount = Random.Range(1, spawnRange);
             for (int i = 0; i < objectCount; i++)
             {
                 Instantiate(Obstacle, new Vector2(transform.position.x, Random.Range(-3, 3)), Quaternion.identity);
