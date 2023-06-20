15caf20e30bc6be5eea85e881ccc729d448fd51f
Pjiwm <pim.munne@gmail.com>
2023-06-20 15:23:59 +0200

    playerrefs in ui with ui prefab

---
+++ Serious-game/Assets/Scripts/StatsManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+public class StatsManager : MonoBehaviour
+{
+    public static string FRIENDPREF = "friends";
+    public static string SWORDPREF = "swordPieces";
+    public TMP_Text friends;
+    public TMP_Text swordPieces;
+
+    private int friendsCount = 0;
+    private int swordPiecesCount = 0;
+
+
+    void Start() {
+        friendsCount = PlayerPrefs.GetInt(FRIENDPREF, 0);
+        swordPiecesCount = PlayerPrefs.GetInt(SWORDPREF, 0);
+    }
+
+    void Update() {
+        friends.text = $"Friends {friendsCount}/3";
+        swordPieces.text = $"Sword Pieces {swordPiecesCount}/3";
+    }
+
+    void requestUpdate() {
+        friendsCount = PlayerPrefs.GetInt(FRIENDPREF, 0);
+        swordPiecesCount = PlayerPrefs.GetInt(SWORDPREF, 0);
+    }
+
+    public static void updatePref(string pref, int value) {
+        PlayerPrefs.SetInt(pref, value);
+        PlayerPrefs.Save();
+    }
+
+
+}
