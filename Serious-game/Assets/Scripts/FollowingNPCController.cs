83ca240941a970f0ab6dc8ad02bf4bc2d2bf60f9
Stijn van Veen <stijnboz@live.nl>
2023-06-22 12:09:06 +0200

    added pathfinding to level 2

--- Serious-game/Assets/Scripts/FollowingNPCController.cs
+++ Serious-game/Assets/Scripts/FollowingNPCController.cs
@@ -1,11 +1,21 @@
 using System;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Pathfinding;
 
 public class FollowingNPCController : MonoBehaviour
 {
     [SerializeField] private Transform playerLocation;
     [SerializeField] private float stopFollowingAtDistance;
+    [SerializeField] private Transform target;
+    [SerializeField] private float nextWaypointDistance = 0.3f;
+    
+    private Path _path;
+    private int _currentWaypoint = 0;
+    private bool _reachedEndOfPath = false;
+    private Seeker _seeker;
+    private Vector2 _direction;
+    
     private NPCController _npcController;
     private MoveController _moveController;
     private Rigidbody2D _rb;
@@ -20,6 +30,7 @@ public class FollowingNPCController : MonoBehaviour
 
     private void Start()
     {
+        _seeker = GetComponent<Seeker>();
         _npcController = GetComponent<NPCController>();
         _moveController = GetComponent<MoveController>();
         _animator = GetComponent<Animator>();
@@ -29,6 +40,7 @@ public class FollowingNPCController : MonoBehaviour
 
         _animator.SetBool(IsWalking, false);
         _npcController.OnDialogFinishedAction += () => { _isFollowingPlayer = true; };
+        InvokeRepeating("UpdatePath", 0f, .5f);
     }
 
     private void Update()
@@ -38,25 +50,64 @@ public class FollowingNPCController : MonoBehaviour
             FollowPlayer();
         }
     }
+    
+    private void UpdatePath()
+    {
+        if (target != null)
+        {
+            if (_seeker.IsDone())
+            {
+                _seeker.StartPath(_rb.position, target.position, OnPathComplete);
+            }
+        }
+        
+    }
+    
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            _path = p;
+            _currentWaypoint = 0;
+        }
+    }
 
     private void FollowPlayer()
     {
-        var collidedObject = Physics2D.OverlapCircle(_rb.position, stopFollowingAtDistance, _playersLayer);
-
-        if (collidedObject)
+        if (_path == null)
         {
-            _animator.SetBool(IsWalking, false);
             return;
         }
+        
+        if (_currentWaypoint >= _path.vectorPath.Count - 1)
+        {
+            _reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            _reachedEndOfPath = false;
+        }
 
+        Vector2 tempDir = ((Vector2)_path.vectorPath[_currentWaypoint] - _rb.position).normalized;
+        if (tempDir != Vector2.zero)
+        {
+            _direction = tempDir;
+        }
+        
+        Vector2 force = _direction * 300f * Time.deltaTime;
+        float distance = Vector2.Distance(_rb.position, _path.vectorPath[_currentWaypoint]);
+        
+        if (distance < nextWaypointDistance)
+        {
+            _currentWaypoint++;
+        }
         if (!_animator.GetBool(IsWalking)) _animator.SetBool(IsWalking, true);
-
-        var nextToPlayerPosition = playerLocation.position;
-
-        var normalizedDirection = nextToPlayerPosition - _npcController.transform.position;
-
-        HandleAnimation(normalizedDirection);
-        _moveController.HandleMovement(normalizedDirection);
+        
+        _rb.AddForce(force);
+        
+        if (!_animator.GetBool(IsWalking)) _animator.SetBool(IsWalking, true);
+        HandleAnimation(_direction.normalized);
     }
 
     private void HandleAnimation(Vector2 inputVector)
