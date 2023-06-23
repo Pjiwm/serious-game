d8664832e83030ef1b1aeee7ea1ae258bcba0b50
Pjiwm <pim.munne@gmail.com>
2023-06-23 15:24:23 +0200

    dialog fixed in emoji shooter and chest

--- Serious-game/Assets/Scripts/Interactables/InteractionDialogManager.cs
+++ Serious-game/Assets/Scripts/Interactables/InteractionDialogManager.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 namespace Interactables
 {
     public class InteractionDialogManager : Singleton<InteractionDialogManager>
     {
+        private void Start()
+        {
+            SceneManager.sceneLoaded += (_, _) =>
+            {
+                HideInteractionDialog();
+            };
+        }
+
         [SerializeField] private GameObject interactionDialogBox;
 
         public void ShowInteractionDialog()
